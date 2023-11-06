47480a56894d1264e712fd6590e9d86a469159ca
Rafał Zubala <rzubala@gmail.com>
2023-11-06 14:35:43 +0100

    tracking support

--- src/main/java/dev/lydtech/dispatch/service/DispatchService.java
+++ src/main/java/dev/lydtech/dispatch/service/DispatchService.java
@@ -1,21 +1,25 @@
 package dev.lydtech.dispatch.service;
 
+import dev.lydtech.dispatch.message.DispatchPreparing;
 import dev.lydtech.dispatch.message.OrderCreated;
 import dev.lydtech.dispatch.message.OrderDispatched;
 import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
-import java.util.concurrent.ExecutionException;
-
 @Service
 @RequiredArgsConstructor
 public class DispatchService {
 
-    public static final String ORDERED_DISPATCHED_TOPIC = "order.dispatched";
+    private static final String DISPATCH_TRACKING_TOPIC = "dispatch.tracking";
+    private static final String ORDER_DISPATCHED_TOPIC = "order.dispatched";
     private final KafkaTemplate<String, Object> kafkaProducer;
+
     public void process(OrderCreated orderCreated) throws Exception {
+        DispatchPreparing dispatchPreparing = DispatchPreparing.builder().orderId(orderCreated.getOrderId()).build();
+        kafkaProducer.send(DISPATCH_TRACKING_TOPIC, dispatchPreparing).get();
+
         OrderDispatched orderDispatched = OrderDispatched.builder().orderId(orderCreated.getOrderId()).build();
-        kafkaProducer.send(ORDERED_DISPATCHED_TOPIC, orderDispatched).get();
+        kafkaProducer.send(ORDER_DISPATCHED_TOPIC, orderDispatched).get();
     }
 }
