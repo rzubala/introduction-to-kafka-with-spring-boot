05fbad8d23c21d3b951df7839192449f62ecdc7f
Rafał Zubala <rzubala@gmail.com>
2023-11-06 16:01:05 +0100

    integration tests

---
+++ src/test/java/dev/lydtech/dispatch/integration/OrderDispatchIntegrationTest.java
@@ -0,0 +1,116 @@
+package dev.lydtech.dispatch.integration;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import dev.lydtech.dispatch.DispatchConfiguration;
+import dev.lydtech.dispatch.message.DispatchPreparing;
+import dev.lydtech.dispatch.message.OrderCreated;
+import dev.lydtech.dispatch.message.OrderDispatched;
+import dev.lydtech.dispatch.util.TestEventData;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.config.KafkaListenerEndpointRegistry;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.kafka.test.EmbeddedKafkaBroker;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.kafka.test.utils.ContainerTestUtils;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+
+import static java.util.UUID.randomUUID;
+import static org.awaitility.Awaitility.await;
+import static org.hamcrest.Matchers.equalTo;
+
+@Slf4j
+@SpringBootTest(classes = {DispatchConfiguration.class})
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+@ActiveProfiles("test")
+@EmbeddedKafka(controlledShutdown = true)
+public class OrderDispatchIntegrationTest {
+
+    private final static String ORDER_CREATED_TOPIC = "order.created";  // input
+    private final static String ORDER_DISPATCHED_TOPIC = "order.dispatched";    // output
+    private final static String DISPATCH_TRACKING_TOPIC = "dispatch.tracking";  // output
+
+    @Autowired
+    private KafkaTemplate kafkaTemplate;
+
+    @Autowired
+    private EmbeddedKafkaBroker embeddedKafkaBroker;
+
+    @Autowired
+    private KafkaListenerEndpointRegistry registry;
+
+    @Autowired
+    private KafkaTestListener testListener;
+
+    @Configuration
+    static class TestConfig {
+
+        @Bean
+        public KafkaTestListener testListener() {
+            return new KafkaTestListener();
+        }
+    }
+
+    /**
+     * Use this receiver to consume messages from the outbound topics.
+     */
+    public static class KafkaTestListener {
+        AtomicInteger dispatchPreparingCounter = new AtomicInteger(0);
+        AtomicInteger orderDispatchedCounter = new AtomicInteger(0);
+
+        @KafkaListener(groupId = "KafkaIntegrationTest", topics = DISPATCH_TRACKING_TOPIC)
+        void receiveDispatchPreparing(@Payload DispatchPreparing payload) {
+            log.debug("Received DispatchPreparing: " + payload);
+            dispatchPreparingCounter.incrementAndGet();
+        }
+
+        @KafkaListener(groupId = "KafkaIntegrationTest", topics = ORDER_DISPATCHED_TOPIC)
+        void receiveOrderDispatched(@Payload OrderDispatched payload) {
+            log.debug("Received OrderDispatched: " + payload);
+            orderDispatchedCounter.incrementAndGet();
+        }
+    }
+
+    @BeforeEach
+    public void setUp() {
+        testListener.dispatchPreparingCounter.set(0);
+        testListener.orderDispatchedCounter.set(0);
+
+        // Wait until the partitions are assigned.
+        registry.getListenerContainers().stream().forEach(container ->
+                ContainerTestUtils.waitForAssignment(container, embeddedKafkaBroker.getPartitionsPerTopic()));
+    }
+
+    /**
+     * Send in an order.created event and ensure the expected outbound events are emitted.
+     */
+    @Test
+    public void testOrderDispatchFlow() throws Exception {
+        OrderCreated orderCreated = TestEventData.buildOrderCreatedData(randomUUID(), "my-item");
+        sendMessage(ORDER_CREATED_TOPIC, orderCreated);
+
+        await().atMost(3, TimeUnit.SECONDS).pollDelay(100, TimeUnit.MILLISECONDS)
+                .until(testListener.dispatchPreparingCounter::get, equalTo(1));
+        await().atMost(1, TimeUnit.SECONDS).pollDelay(100, TimeUnit.MILLISECONDS)
+                .until(testListener.orderDispatchedCounter::get, equalTo(1));
+    }
+
+    private void sendMessage(String topic, Object data) throws Exception {
+        kafkaTemplate.send(MessageBuilder
+                .withPayload(data)
+                .setHeader(KafkaHeaders.TOPIC, topic)
+                .build()).get();
+    }
+}
